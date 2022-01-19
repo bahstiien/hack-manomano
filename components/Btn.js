c57b3b5b074020f8a5b8f7d35e0f2ab3b89ca714
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-01-19 22:24:49 +0100

    add button with props

---
+++ components/Btn.js
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+export default function BasicButtons({content}) {
+  return (
+    <>
+      <Button variant="contained">{content}</Button>
+    </>
+  );
+}
