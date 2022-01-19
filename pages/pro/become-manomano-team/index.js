b2fc1367c5573708c3d79146f1aab64110036313
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-01-19 23:32:07 +0100

    form new artisan ok on first version simple

--- pages/pro/become-manomano-team/index.js
+++ pages/pro/become-manomano-team/index.js
@@ -4,33 +4,44 @@ export default function FormArtisan() {
   return (
     <>
       <h1 className="text-five"> Devenir Artisan @ManoMano </h1>
-      <form className={style.formArtisan}>
+      <form className={style.createArtisanForm}>
         <h2 className={style.formTitle}>Créer votre fiche profil</h2>
 
-        <button className={style.btnUpload}>UPLOAD</button>
-        <div className={style.nameContainer}>
-          <h3 className={style.littleTitle}>Nom</h3>
-          <input
-            type="text"
-            id="name"
-            name="UserName"
-            placeholder="User Name"
-            autoComplete="off"
-          />
-          <h3 className={style.littleTitle}>Prénom</h3>
-          <input
-            type="text"
-            id="name"
-            name="UserName"
-            placeholder="User Name"
-            autoComplete="off"
+        <div className={style.fullNameContainer}>
+          <div className={style.nameContainer}>
+            <h3 className={style.inputTitleForm}>Nom</h3>
+            <input
+              type="text"
+              className={style.nameInputForm}
+              autoComplete="off"
+            />
+          </div>
+          <div className={style.nameContainer}>
+            <h3 className={style.inputTitleForm}>Prénom</h3>
+            <input
+              type="text"
+              className={style.nameInputForm}
+              autoComplete="off"
+            />
+          </div>
+        </div>
+        <div className={style.descriptionContainer}>
+          <h3 className={style.inputTitleForm}>Decription</h3>
+          <textarea
+            name="descriptio "
+            className={style.textAreaForm}
+            required
           />
         </div>
 
-        <h3 className={style.littleTitle}>Decription</h3>
-        <textarea name="new-post" id="new-post-area" required />
+        <div className={style.uploadImageContainer}>
+          <h3 className={style.inputTitleForm}>
+            Téléchargez votre photo de profil
+          </h3>
+          <button className={style.uploadPhotoForm}>Choisir Photo</button>
+        </div>
 
-        <button type="submit" id="add-post-btn">
+        <button type="submit" className={style.btnSubmitForm}>
           Envoyer le formulaire
         </button>
       </form>
