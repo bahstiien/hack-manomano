05e6fbec75bcd5c89159f3e268ce2e6223f7a522
bahstiien <bastien.lecalvez@gmail.com>
2022-01-19 15:12:21 +0100

    WIP

---
+++ components/serviceSearch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ServiceSearch = () => {
+  return (
+    <div>
+      <input type="text" className="w-1/4 h-8 border-2 rounded" />
+    </div>
+  );
+};
+
+export default ServiceSearch;
---
+++ pages/service.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ServiceSearch from "../components/serviceSearch";
+
+const service = () => {
+  return (
+    <div>
+      <h1> Service </h1>
+      <ServiceSearch />
+    </div>
+  );
+};
+
+export default service;
+<h1> Service </h1>;
