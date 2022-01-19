ebf5cc46d3b10b2649d5da7f54cd0d793a1d5424
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-01-19 22:02:56 +0100

    add my projet folder

---
+++ pages/user/myproject/index.js
@@ -0,0 +1,11 @@
+export default function MyProject() {
+  return (
+    <>
+      <div className="text-center text-3xl font-bold">Mes projets</div>
+
+      <p className="text-center"> Salle de bain</p>
+
+      
+    </>
+  );
+}
