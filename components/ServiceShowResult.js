11aca33d208f77e9ed05607aec814fd539f31d3c
bahstiien <bastien.lecalvez@gmail.com>
2022-01-20 13:11:25 +0100

    finish work on details

--- components/ServiceShowResult.js
+++ components/ServiceShowResult.js
@@ -13,29 +13,39 @@ import "swiper/css/scrollbar";
 const ServiceShowResult = () => {
   return (
     <div>
-      <h1 className="text-2xl text-one font-bold mb-5">
+      <h1 className="text-5xl font-second text-one font-bold mb-5">
         Mes dernieres réalisations
       </h1>
-      <Swiper
-        scrollbar={{
-          hide: false,
-        }}
-        className="mySwiper"
-      >
+      <Swiper className="mySwiper">
         <SwiperSlide>
-          <Image src={bathroom1} width="640x" height="426px" />
+          <Image src={bathroom1} width="640x" height="426px" alt="Project" />
+          <p className="text-six text-lg mb-4">
+            Rénovation de la salle de bain de chez Bruno
+          </p>
         </SwiperSlide>
         <SwiperSlide>
-          <Image src={bathroom2} width="640x" height="426px" />
+          <Image src={bathroom2} width="640x" height="426px" alt="Project" />
+          <p className="text-six text-lg mb-4">
+            Rénovation de la salle de bain de chez Marie
+          </p>
         </SwiperSlide>
         <SwiperSlide>
-          <Image src={bathroom3} width="640x" height="426px" />
+          <Image src={bathroom3} width="640x" height="426px" alt="Project" />
+          <p className="text-six text-lg mb-4">
+            Nouvelle baignoire chez Laurent
+          </p>
         </SwiperSlide>
         <SwiperSlide>
-          <Image src={kitchen1} width="640x" height="426px" />
+          <Image src={kitchen1} width="640x" height="426px" alt="Project" />
+          <p className="text-six text-lg mb-4">
+            Agencement de la cuisine de Bruno
+          </p>
         </SwiperSlide>
         <SwiperSlide>
-          <Image src={kitchen2} width="640x" height="426px" />
+          <Image src={kitchen2} width="640x" height="426px" alt="Project" />
+          <p className="text-six text-lg mb-4">
+            Rénovation de la cuisine de chez Sergueï
+          </p>
         </SwiperSlide>
       </Swiper>
     </div>
--- pages/service/index.js
+++ pages/service/index.js
@@ -8,78 +8,83 @@ import pax3 from "../../public/worker/pax3.jpg";
 import pax4 from "../../public/worker/pax4.jpg";
 import pax5 from "../../public/worker/pax5.jpg";
 import pax6 from "../../public/worker/pax6.jpg";
+import Link from "next/link";
+import Layout from "../../components/Layout";
 
 const service = () => {
   return (
-    <div>
-      <h1 className="text-five"> Service </h1>
-      <ServiceSearch />
+    <Layout>
+      <div className="mb-24">
+        <h1 className="text-five"> Service </h1>
+        <ServiceSearch />
 
-      <div className="flex justify-around flex-wrap">
-        <ServiceResult
-          activity="Plomberie"
-          img={pax1}
-          note="4.1"
-          nb="18"
-          name="Jean Michel Pommeau"
-          city="Lyon 3e"
-          description="Passionné de fuite d'eau depuis mon enfance j'en ai fait mon métier"
-          price={"15"}
-        />
-        <ServiceResultNop
-          img={pax6}
-          activity="Rénovation"
-          note="4.7"
-          nb="8"
-          name="Therese Tapalo"
-          city="GIVORS"
-          description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
-          price={"35"}
-        />
-        <ServiceResultNop
-          img={pax2}
-          activity="Rénovation"
-          note="4.7"
-          nb="8"
-          name="Therese Tapalo"
-          city="GIVORS"
-          description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
-          price={"35"}
-        />
-        <ServiceResult
-          img={pax4}
-          activity="Rénovation"
-          note="4.7"
-          nb="8"
-          name="Therese Tapalo"
-          city="GIVORS"
-          description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
-          price={"35"}
-        />
-        <ServiceResult
-          img={pax3}
-          activity="Rénovation"
-          note="4.7"
-          nb="8"
-          name="Therese Tapalo"
-          city="GIVORS"
-          description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
-          price={"35"}
-        />
-        <ServiceResult
-          img={pax5}
-          activity="Bricolage"
-          note="3.7"
-          nb="2"
-          name="Francis De La Fuit'Do"
-          city="LYON 12e"
-          description="Je suis toujours sous l'eau mais j'aime mon travail et rendre service aux gens"
-          price={"25"}
-        />
+        <div className="flex justify-around flex-wrap">
+          <ServiceResult
+            activity="Plomberie"
+            img={pax1}
+            note="4.1"
+            nb="18"
+            name="Jean Michel Pommeau"
+            city="Lyon 3e"
+            description="Passionné de fuite d'eau depuis mon enfance j'en ai fait mon métier"
+            price={"15"}
+          />
+          <ServiceResultNop
+            img={pax6}
+            activity="Rénovation"
+            note="4.7"
+            nb="8"
+            name="Therese Tapalo"
+            city="GIVORS"
+            description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
+            price={"35"}
+          />
+          <ServiceResultNop
+            img={pax2}
+            activity="Rénovation"
+            note="4.7"
+            nb="8"
+            name="Therese Tapalo"
+            city="GIVORS"
+            description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
+            price={"35"}
+          />
+
+          <ServiceResult
+            img={pax4}
+            activity="Rénovation"
+            note="4.7"
+            nb="8"
+            name="Therese Tapalo"
+            city="GIVORS"
+            description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
+            price={"35"}
+          />
+
+          <ServiceResult
+            img={pax3}
+            activity="Rénovation"
+            note="4.7"
+            nb="8"
+            name="Therese Tapalo"
+            city="GIVORS"
+            description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
+            price={"35"}
+          />
+          <ServiceResult
+            img={pax5}
+            activity="Bricolage"
+            note="3.7"
+            nb="2"
+            name="Francis De La Fuit'Do"
+            city="LYON 12e"
+            description="Je suis toujours sous l'eau mais j'aime mon travail et rendre service aux gens"
+            price={"25"}
+          />
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
 export default service;
-<h1> Service </h1>;
