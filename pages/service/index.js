505e2dfb900f08158dfcf26c5dd004cf9f9900ca
bahstiien <bastien.lecalvez@gmail.com>
2022-01-20 13:31:12 +0100

    finish work on details

--- pages/service/index.js
+++ pages/service/index.js
@@ -27,27 +27,27 @@ const service = () => {
             name="Jean Michel Pommeau"
             city="Lyon 3e"
             description="Passionné de fuite d'eau depuis mon enfance j'en ai fait mon métier"
-            price={"15"}
+            price={"45"}
           />
           <ServiceResultNop
             img={pax6}
-            activity="Rénovation"
-            note="4.7"
-            nb="8"
-            name="Therese Tapalo"
-            city="GIVORS"
-            description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
+            activity="Bricolage - Plomberie"
+            note="2.9"
+            nb="2"
+            name="Véronique Sartre"
+            city="VILLEURBANNE"
+            description="Je m'occupe de petites taches de réparation sur la plomberie"
             price={"35"}
           />
           <ServiceResultNop
             img={pax2}
-            activity="Rénovation"
-            note="4.7"
-            nb="8"
-            name="Therese Tapalo"
-            city="GIVORS"
-            description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
-            price={"35"}
+            activity="Rénovation - plomberie"
+            note="4.5"
+            nb="3"
+            name="Auguste Reverdin"
+            city="VAUX EN VELIN"
+            description="Plombier depuis 15 ans je suis en mesure de vous aider dans la réalisation de vos projets"
+            price={"40"}
           />
 
           <ServiceResult
@@ -57,29 +57,29 @@ const service = () => {
             nb="8"
             name="Therese Tapalo"
             city="GIVORS"
-            description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
-            price={"35"}
+            description="Je suis passionnée de rénovation, je suis architecte d'intérieur, disponible et à votre écoute"
+            price={"65"}
           />
 
           <ServiceResult
             img={pax3}
-            activity="Rénovation"
-            note="4.7"
-            nb="8"
-            name="Therese Tapalo"
+            activity="Plomberie"
+            note="3.9"
+            nb="4"
+            name="Michel Lazard"
             city="GIVORS"
             description="Pourquoi mettre une description ? Ce que je veux c'est réparer ta douche"
-            price={"35"}
+            price={"90"}
           />
-          <ServiceResult
+          <ServiceResultNop
             img={pax5}
             activity="Bricolage"
-            note="3.7"
+            note="2.7"
             nb="2"
             name="Francis De La Fuit'Do"
             city="LYON 12e"
             description="Je suis toujours sous l'eau mais j'aime mon travail et rendre service aux gens"
-            price={"25"}
+            price={"15"}
           />
         </div>
       </div>
