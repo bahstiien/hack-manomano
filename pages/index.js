52cf463b0e02db6ea3e0c6ca9b788918e382a64f
bahstiien <bastien.lecalvez@gmail.com>
2022-01-20 21:36:44 +0100

    WIP home

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,41 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
-// import logo from "../public/logoManoMano.png";
+import logo from "../public/logoManoMano.png";
 import image1 from "../public/image-01.jpeg";
 import image2 from "../public/image-02.jpeg";
 import image3 from "../public/image-03.jpeg";
+import ReactPlayer from "react-player";
 
 export default function Home() {
   return (
     <Layout pageTitle="Mano Mano la communauté">
       <div>
-        {/* <div className={styles.container}>
+        <div className={styles.container}>
           <Image className="" src={logo} alt="logo" layout="responsive" />
-        </div> */}
+        </div>
 
         <link
           rel="stylesheet"
           href="https://cdn.tailgrids.com/tailgrids-fallback.css"
         />
 
+        {/* <div className="flex justify-center">
+          <iframe
+            frameborder="0"
+            allow="autoplay; fullscreen;"
+            allowfullscreen
+            className="w-2/3 h-screen"
+            src="https://www.flexclip.com/embed/8e8b7d2755ae2e651a21e66c91ee39fb.html"
+          />
+        </div> */}
         <section className="pt-20 lg:pt-[120px] pb-10 lg:pb-20 bg-[#F3F4F6]">
           <div className="container">
             <div className="flex flex-wrap -mx-4">
               <div className="w-full md:w-1/2 xl:w-1/3 px-4">
                 <div className="bg-white rounded-lg overflow-hidden mb-10">
                   <Image src={image1} alt="image1" className="w-full" />
+
                   <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
                     <h3>
                       <a
